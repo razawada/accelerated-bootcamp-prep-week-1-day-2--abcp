ac06d29a51937b1a080d5efba72aa230551e19e4
Rance Zawada <razawada@gmail.com>
2018-01-14 18:33:33 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,34 +1,42 @@
 function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
  // add the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
   // add element to the beginning of the array by mutating the array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 
 }
 
 function removeElementFromEndOfArray(array) {
-
+  return array.slice(0,array.length-1)
 }
